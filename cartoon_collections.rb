36dc65bded15fa23081da66688d871757ef7671c
Kachel Wittig <kjwittig@gmail.com>
2018-03-15 03:33:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(name)
 end
 
 def summon_captain_planet(planeteer)
-  planeteer.collect { |x| puts x.capitalize + "!"}
+  planeteer.collect { |x| x.capitalize + "!"}
 end
 
 def long_planeteer_calls
