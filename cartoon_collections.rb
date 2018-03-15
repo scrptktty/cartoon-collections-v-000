578201ac195f835f0aef04beee972ac4f9ee8f3e
Kachel Wittig <kjwittig@gmail.com>
2018-03-15 04:52:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,9 +23,17 @@ def long_planeteer_calls(calls)
 
 end
 
+# def find_the_cheese(food)
+#   cheese_arr = ["cheddar", "gouda", "camembert"]
+# 
+#   (cheese_arr & food).first
+#   binding.pry
+# end
+
+
 def find_the_cheese(food)
   cheese_arr = ["cheddar", "gouda", "camembert"]
 
-  (cheese_arr & food).first
-  binding.pry
+  cheese_arr.include?(food)
+
 end
